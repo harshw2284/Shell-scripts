73ece6c206184801827c1a365b74fb7f228c813c
Harsh Bhagat <harshbhagat0079@gmail.com>
2026-03-27 23:30:22 +0530

    Update hello.sh

--- Day-01/hello.sh
+++ Day-01/hello.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#Task 1: Your First Script
+#Create a file hello.sh
+#Add the shebang line (#!/bin/bash at the top)
+#print Hello, DevOps! using echo
+#Make it executable and run it
 
 echo "Hello, Devops!"
