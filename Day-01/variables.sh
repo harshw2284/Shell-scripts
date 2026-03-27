9cecf367ce490ea90e756781ab519f9b878b7508
Harsh Bhagat <harshbhagat0079@gmail.com>
2026-03-27 23:31:29 +0530

    Update variables.sh

--- Day-01/variables.sh
+++ Day-01/variables.sh
@@ -1,3 +1,11 @@
+#Create variables.sh with:
+#A variable for your NAME
+#A variable for your ROLE (e.g., "DevOps Engineer")
+#Print: Hello, I am <NAME> and I am a <ROLE>
+#Try using single quotes vs double quotes — what's the difference?
+
+
+
 #!/bin/bash
 
 read -p 'Enter your Name: ' NAME
